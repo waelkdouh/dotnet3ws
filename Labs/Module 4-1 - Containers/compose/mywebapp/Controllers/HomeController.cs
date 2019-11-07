678b530a9092f4b92074c42ee2ca6731247c9667
Wael Kdouh <waekdo@microsoft.com>
2019-11-06 22:05:19 -0500

    Added containers labs

---
+++ Labs/Module 4-1 - Containers/compose/mywebapp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mywebapp.Models;
+
+namespace mywebapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        HttpClient client;
+
+        public HomeController()
+        {
+            client = new HttpClient();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Quotes()
+        {
+
+            var response = await client.GetStringAsync("http://demowebapi:9000/api/quotes");
+            ViewData["Message"] = response; 
+
+            return View();
+        }
+    }
+}
